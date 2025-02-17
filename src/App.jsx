2d57d3c8b3498e73c2e74d4a41ede72f01b9cf19
abhishekgarad5677 <abhishekg@neelafilm.com>
2025-02-17 12:24:38 +0530

    Add: navbar

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-[url(../../public/background-cover.png)] h-[1000vh] bg-[#0095ff] bg-cover bg-center bg-no-repeat">
+        <Navbar />
+        <div className='bg'>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
